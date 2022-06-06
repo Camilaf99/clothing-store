6db715940a0145b48dd7091393bc8fa1ca0e4a9a
Camila Fernandez <ailenfernandez99@gmail.com>
2022-06-06 17:50:07 -0500

    added new categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,31 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({ include: Product }).then(function (categories) {
+    res.json(categories);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const category = Category.findByPk(req.params.id, { include: Product }).then(function (category) {
+    res.json(category);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  /*
+    {
+      categoryName: ""
+    }
+  */
+    const { categoryName } = req.body;
+    Category.create({ category_name: categoryName }).then(function (newCategory) {
+      res.location('/api/categories/' + newCategory.id);
+      res.sendStatus(201);  
+    });
 });
 
 router.put('/:id', (req, res) => {
